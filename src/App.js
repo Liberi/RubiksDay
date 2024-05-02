fff950921022502e46134de359731ada2a185bf0
Lib_int <7102axe@gmail.com>
2024-05-03 00:12:35 +0300

    Исправление ошибок

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Будущая страница разработки сайта для Дня Кубика-рубика
+          <b>Будущая страница разработки сайта
+           для Дня Кубика-рубика</b>
         </p>
         <a
           className="App-link"
