f53e599ebdeb7972fea4cc5a0cbeaba62c7e38e8
Lib_int <7102axe@gmail.com>
2024-05-02 23:59:14 +0300

    Визуальное временное отображение страницы

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload!
+          Будущая страница разработки сайта для Дня Кубика-рубика
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Liberi/rubiksDay"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          На GinHub
         </a>
       </header>
     </div>
