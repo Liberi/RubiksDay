3720ebe8bc530675846a8da163f0c248d679088f
Liberi <7102axe@gmail.com>
2024-05-09 23:20:59 +0300

    Обновление под адаптив heder-а

--- src/components/heder/heder.js
+++ src/components/heder/heder.js
@@ -5,15 +5,18 @@ import MainLogo from "../../assets/img/back/MainLogo.png";
 
 export default function PageNotFound() {
   return (
-    <header>
+    <header className="flex">
       <a href="/" className="mainLogo">
         <img src={MainLogo} alt="NotFound" />
       </a>
-      {DataHeder.map((item) => (
-        <a href={item.link} style={item.style} key={item.id}>
-          {item.linkText}
-        </a>
-      ))}
+      <div className="blockLink flex">
+        {DataHeder.map((item) => (
+          <a href={item.link} style={item.style} key={item.id}>
+            {item.linkText}
+          </a>
+        ))}
+      </div>
+      <button className="menuHeder" />
     </header>
   );
 }
